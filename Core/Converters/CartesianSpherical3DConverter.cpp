5df6304719ef8bdcafd8b1ea395f9dd9cf4770f4
Mostafa Palizdar <mostafa.palizdar@gmail.com>
2025-04-11 21:19:14 +0200

    finish cartesian polar converters

---
+++ Core/Converters/CartesianSpherical3DConverter.cpp
@@ -0,0 +1,8 @@
+//
+//  CartesianPolar3DConverter.cpp
+//  TinyMath3D
+//
+//  Created by Mostafa on 10.04.25.
+//
+
+#include "CartesianPolar3DConverter.hpp"
---
+++ Core/Converters/CartesianSpherical3DConverter.hpp
@@ -0,0 +1,25 @@
+//
+//  CartesianPolar3DConverter.hpp
+//  TinyMath3D
+//
+//  Created by Mostafa on 10.04.25.
+//
+#pragma once
+
+#include <stdio.h>
+
+namespace TinyMath3D::Converters {
+struct Cartesian3DCoordinates {
+	float x;
+	float y;
+	float z;
+};
+float Polar3DCoordinates {
+	float r;
+	float heading;
+	float pitch;
+};
+class CartesianPolar3DConverter {
+	static 
+};
+} // namespace TinyMath3D::Converters
