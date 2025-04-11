1f509dfd851971029aca4cd5be56724cceddf7a8
Mostafa Palizdar <mostafa.palizdar@gmail.com>
2025-04-11 21:20:10 +0200

    added cartesian polar converters

--- Core/Converters/CartesianSpherical3DConverter.cpp
+++ Core/Converters/CartesianSpherical3DConverter.cpp
@@ -5,4 +5,36 @@
 //  Created by Mostafa on 10.04.25.
 //
 
-#include "CartesianPolar3DConverter.hpp"
+#include "CartesianSpherical3DConverter.hpp"
+#include <cmath>
+
+namespace TinyMath3D::Converters {
+static Cartesian3DCoordinates SphericalToCartesian3D(const float &r,
+                                                     const float &heading,
+                                                     const float &pitch) {
+	Cartesian3DCoordinates result;
+	result.x = r * cos(pitch) * sin(heading);
+	result.y = -r * sin(pitch);
+	result.z = r * cos(pitch) * cos(heading);
+	return result;
+}
+static Spherical3DCoordinates
+CartesianToSpherical3D(const float &x, const float &y, const float &z) {
+	Spherical3DCoordinates result;
+	result.r = sqrt(x * x + y * y + z * z);
+	if (result.r < std::numeric_limits<double>::epsilon())
+		return {0.0, 0.0, 0.0};
+
+	result.pitch = asin(-y / result.r);
+
+	if (std::fabs(result.pitch) >= PI_OVER_TWO * 0.9999) {
+		// Gimbal lock: heading is undefined when looking straight up/down
+		result.heading = 0.0;
+	} else {
+		result.heading = atan2(x, z); // Left-handed: X → Z
+	}
+	result.heading = NormalizeAngle2Pi(result.heading);
+	return result;
+}
+
+} // namespace TinyMath3D::Converters
--- Core/Converters/CartesianSpherical3DConverter.hpp
+++ Core/Converters/CartesianSpherical3DConverter.hpp
@@ -9,17 +9,42 @@
 #include <stdio.h>
 
 namespace TinyMath3D::Converters {
+
+constexpr float PI = 3.14159265358979323846;
+constexpr float TWO_PI = 2.0 * PI;
+constexpr float PI_OVER_TWO = PI / 2.0;
+
 struct Cartesian3DCoordinates {
 	float x;
 	float y;
 	float z;
 };
-float Polar3DCoordinates {
+struct Spherical3DCoordinates {
 	float r;
 	float heading;
 	float pitch;
 };
-class CartesianPolar3DConverter {
-	static 
+class CartesianSpherical3DConverter {
+	static Cartesian3DCoordinates SphericalToCartesian3D(const float &r,
+	                                                     const float &heading,
+	                                                     const float &pitch);
+	inline static Cartesian3DCoordinates
+	SphericalToCartesian3D(const Spherical3DCoordinates &coordinates) {
+		return SphericalToCartesian3D(coordinates.r, coordinates.heading,
+		                              coordinates.pitch);
+	}
+
+	static Spherical3DCoordinates
+	CartesianToSpherical3D(const float &x, const float &y, const float &z);
+	static Spherical3DCoordinates
+	CartesianToSpherical3D(const Cartesian3DCoordinates);
+
+	inline static float NormalizeAngle2Pi(float angle) {
+		while (angle < 0.0)
+			angle += TWO_PI;
+		while (angle >= TWO_PI)
+			angle -= TWO_PI;
+		return angle;
+	}
 };
 } // namespace TinyMath3D::Converters
